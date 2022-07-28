6ca797d52d39dc6c1632ab5306b088e9abd1452e
Margaret Sun <hi.margaret.sun@gmail.com>
2022-07-28 12:13:22 -0700

    add survey questions and inputs to /begin

--- app.py
+++ app.py
@@ -7,3 +7,27 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get("/begin")
+def display_survey():
+    """Display survey start template"""
+    title = survey.title
+    instructions = survey.instructions
+
+    # turning survey questions into dictionary
+    # questions = {}
+    # index = 0
+    # for question in survey.questions:
+    #     questions[index] = question.question
+    #     index += 1
+
+    questions = []
+    for question in survey.questions:
+        questions.append(question)
+    print (questions)
+
+    return render_template("survey_start.html", title=title,
+                           instructions=instructions, questions=questions)
