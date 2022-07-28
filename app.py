12b544032d688f26e58e5e89b4ba997be2e45ae9
Brenda Liu <brenda.liu24@gmail.com>
2022-07-28 18:37:52 -0500

    finished survey making steps

--- app.py
+++ app.py
@@ -11,23 +11,58 @@ debug = DebugToolbarExtension(app)
 responses = []
 
 
-@app.get("/begin")
-def display_survey():
-    """Display survey start template"""
+@app.get("/")
+def display_start():
+    """Display survey start page template"""
+
     title = survey.title
     instructions = survey.instructions
 
-    # turning survey questions into dictionary
-    # questions = {}
-    # index = 0
-    # for question in survey.questions:
-    #     questions[index] = question.question
-    #     index += 1
-
-    questions = []
-    for question in survey.questions:
-        questions.append(question)
-    print (questions)
-
     return render_template("survey_start.html", title=title,
-                           instructions=instructions, questions=questions)
+                           instructions=instructions)
+
+
+
+@app.post('/begin')
+def display_question():
+    '''redirect user to first question'''
+    responses = []
+
+    return redirect('/question/0')
+
+
+@app.get('/question/<index>')
+def display_single_question(index):
+    '''display question with choices depending on index'''
+
+    questions = survey.questions
+    question = questions[int(index)]
+
+
+    return render_template('question.html', question = question)
+
+
+
+@app.post("/answer")
+def get_response():
+    '''append response from each question to responses list
+    and redirect to next question'''
+
+    answer = request.form["answer"]
+    responses.append(answer)
+
+    if len(responses) == len(survey.questions):
+
+        redirect_page = '/completion'
+    else:
+        index = len(responses)
+        redirect_page = f'/question/{index}'
+
+    return redirect(redirect_page)
+
+
+@app.get('/completion')
+def show_completion():
+    '''display thank you completion page'''
+
+    return render_template('completion.html')
