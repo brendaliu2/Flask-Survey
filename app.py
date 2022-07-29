37991f0a9ad8d49a091c30a5f13fb88cf51995fb
Margaret Sun <hi.margaret.sun@gmail.com>
2022-07-28 17:10:19 -0700

    finish step 7

--- app.py
+++ app.py
@@ -8,38 +8,36 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+
 
 
 @app.get("/")
 def display_start():
     """Display survey start page template"""
-
-    title = survey.title
-    instructions = survey.instructions
-
-    return render_template("survey_start.html", title=title,
-                           instructions=instructions)
+    session["responses"] = []
+    return render_template("survey_start.html", survey = survey)
 
 
 
 @app.post('/begin')
 def display_question():
     '''redirect user to first question'''
-    responses = []
 
     return redirect('/question/0')
 
 
-@app.get('/question/<index>')
+@app.get('/question/<int:index>')
 def display_single_question(index):
     '''display question with choices depending on index'''
 
     questions = survey.questions
-    question = questions[int(index)]
 
-
-    return render_template('question.html', question = question)
+    if index != len(session["responses"]):
+        index = len(session["responses"])
+        return redirect(f'/question/{index}')
+    else:
+        question = questions[index]
+        return render_template('question.html', question = question)
 
 
 
@@ -49,13 +47,16 @@ def get_response():
     and redirect to next question'''
 
     answer = request.form["answer"]
-    responses.append(answer)
 
-    if len(responses) == len(survey.questions):
+    responses = session["responses"]
+    responses.append(answer)
+    session["responses"] = responses
+
+    if len(session["responses"]) == len(survey.questions):
 
         redirect_page = '/completion'
     else:
-        index = len(responses)
+        index = len(session["responses"])
         redirect_page = f'/question/{index}'
 
     return redirect(redirect_page)
@@ -66,3 +67,5 @@ def show_completion():
     '''display thank you completion page'''
 
     return render_template('completion.html')
+
+# redirect to completion or question/index
